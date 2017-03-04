96159d5838476d7bf577d4c41329ea19e6469e87
Muhammad Eakub <meakub86@gmail.com>
2017-03-04 00:31:16 -0500

    modified parentheses checker file to use python class

--- DSAndAlgorithms/parentheseschecker.py
+++ DSAndAlgorithms/parentheseschecker.py
@@ -1,28 +1,38 @@
 from DSAndAlgorithms.stack import Stack
 
 
-def parChecker(symbolStr):
-    print("Given symbol string : ", symbolStr)
-    s = Stack()
-    balanced = True
-    index = 0
-    while index < len(symbolStr) and balanced:
-        ltrl = symbolStr[index]
-        if ltrl == '(':
-            s.push(ltrl)
-        else:
-            if s.isempty():
-                balanced = False
-            else:
-                s.pop()
-        index += 1
-    if balanced and s.isempty():
-        return True
-    else:
-        return False
+class ParenthesesChecker:
+    def __init__(self):
+        self.symbolStr = ""
+    """
+    this class use Stack class from stack module and help checking balanced parentheses
+    """
 
-print(parChecker("(((())()())))"))
-print(parChecker("()()((((((((((((()))))))))))))"))
+    def Check(self, symbolStr):
+        print("Given symbol string : ", symbolStr)
+        s = Stack()
+        balanced = True
+        index = 0
+        while index < len(symbolStr) and balanced:
+            symbol = symbolStr[index]
+            if symbol == '(':
+                s.push(symbol)
+            else:
+                if s.isempty():
+                    balanced = False
+                else:
+                    s.pop()
+            index += 1
+        if balanced and s.isempty():
+            return True
+        else:
+            return False
+
+if __name__ == '__main__':
+    obj = ParenthesesChecker()
+    print(obj.Check("(((())()())))"))
+    print(obj.Check("()()((((((((((((()))))))))))))"))
+
 
 
 
