1cc92a7df5e10bdcdcff11d4eb822ddc5a029b5d
Muhammad Eakub <meakub86@gmail.com>
2017-03-08 10:57:24 -0500

    some list operations

---
+++ Collections/list.py
@@ -0,0 +1,13 @@
+list = "this is just a string".split()
+print("list = ", list)
+print("list[2] = ", list[2]) # 3rd element - just
+print("list[-4] = ", list[-4]) # is
+#negative index in based on minus one
+print("list[-0] = ", list[-0]) # -0 and 0 both point to first items
+
+print("list[1:3] = ", list[1:3]) # item within position 1 to 4
+print("list[:2] = ", list[:2])
+print("list[3:] = ",list[3:])
+print("list[:] = ", list[:])
+
+
