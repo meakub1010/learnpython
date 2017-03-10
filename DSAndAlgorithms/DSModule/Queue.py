a6f9d537954468fbf25ce37563a38d51f3170234
Muhammad Eakub <meakub86@gmail.com>
2017-03-10 00:11:00 -0500

    added hot potato a queue example

--- DSAndAlgorithms/DSModule/Queue.py
+++ DSAndAlgorithms/DSModule/Queue.py
@@ -2,10 +2,10 @@ class Queue:
     def __init__(self):
         self.items = []
 
-    def enque(self, item):
+    def enqueue(self, item):
         self.items.insert(0, item)
 
-    def deque(self):
+    def dequeue(self):
         return self.items.pop()
 
     def size(self):
@@ -19,10 +19,10 @@ class Queue:
 
 if __name__ == '__main__':
     q = Queue()
-    q.enque(13)
-    q.enque('True')
-    q.enque(4.5)
-    # print(q.deque())
+    q.enqueue(13)
+    q.enqueue('True')
+    q.enqueue(4.5)
+    # print(q.dequeue())
     print("size = ", q.size())
     print(q)
     print(q.isempty())
