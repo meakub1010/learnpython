9583a0df0fa40f74a17bd9a006ed2d00824f055f
Muhammad Eakub <meakub86@gmail.com>
2017-03-14 11:33:00 -0400

    modified air travel

--- Classes/airtravel.py
+++ Classes/airtravel.py
@@ -3,8 +3,43 @@
 
 class Flight:
 
-    def __init__(self, number):
-        self._number = number # 1. _ used to avoid name clash with method named ""
+    def __init__(self, number, aircraft):
+        # print(number[:2])
+        if not number[:2].isalpha():
+            raise ValueError("No airline code in '{}'".format(number))
+
+        if not number[:2].isupper():
+            raise ValueError("Invalid airline code '{}'".format(number))
+
+        if not (number[2:].isdigit() and int(number[2:]) <= 999):
+            raise ValueError("Invalid route number '{}'".format(number))
+
+        self._number = number  # 1. _ used to avoid name clash with method named ""
+        self._aircraft = aircraft
 
     def number(self):
-        return "SN060"
+        return self._number
+
+    def airline(self):
+        return self._number[:2]
+
+    def aircraft_model(self):
+        return self._aircraft.model()
+
+
+class Aircraft:
+    def __init__(self, registration, model, num_rows, num_seats_per_row):
+        self._registration = registration
+        self._model = model
+        self._num_rows = num_rows
+        self._num_seats_per_row = num_seats_per_row
+
+    def registration(self):
+        return self._registration
+
+    def model(self):
+        return self._model
+
+    def seating_plan(self):
+        return range(1, self._num_rows + 1), "ABCDEFGHJK"[:self._num_seats_per_row]
+
