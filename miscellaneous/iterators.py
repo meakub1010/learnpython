5e1a68a1da3182f88ad80d9978fafc67556828b6
Muhammad Eakub <meakub86@gmail.com>
2017-03-29 09:45:25 -0400

    iterators practice

---
+++ miscellaneous/iterators.py
@@ -0,0 +1,15 @@
+"""
+iterating through collection
+"""
+
+for element in [1, 2, 3, 4]:
+    print(element)
+
+for element in (1, 2, 3, 4):
+    print(element)
+
+for key in {'k1': 'val1', "k2" : "val2"}:
+    print(key)
+
+for char in "ABC4":
+    print (char)
