8854db78291c9403c19af1d68904324e228dcbc0
Muhammad Eakub <meakub86@gmail.com>
2017-03-29 09:45:47 -0400

    modified trial n error

--- miscellaneous/trialnerror.py
+++ miscellaneous/trialnerror.py
@@ -1,24 +1,18 @@
-def foo(param1, param2):
-    """
-    this is function and not associated with any object/instance. can be called directly with any instance
-    :param param1:
-    :param param2:
-    :return:
-    """
-    return param1 + " " + param2
+class B(Exception):
+    pass
 
+class C(B):
+    pass
 
-class Foo:
-    def foo(self, param1, param2):
-        """
-        this is method associated with the object of Foo class
-        :param param1: anything
-        :param param2: anything
-        :return: concatenate params
-        """
-        return param1 + " " + param2
+class D(C):
+    pass
 
-
-if __name__ == '__main__':
-    print(foo("hi", "there"))
-    print(Foo().foo("hi", "there!"))
+for c in [B, C, D]:
+    try:
+        raise c()
+    except D:
+        print("D")
+    except C:
+        print("C")
+    except B:
+        print("B")
